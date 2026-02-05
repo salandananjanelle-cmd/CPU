ec1c05b0b02f0025e708c94ed3973cea51179c30
cyrusalacar05 <cyru3z@gmail.com>
2026-02-05 17:57:01 +0800

    Create Keyboard.java

---
+++ Keyboard.java
@@ -0,0 +1,14 @@
+public class Keyboard {
+
+    private String type;
+    private int numberOfKeys;
+
+    public Keyboard(String type, int numberOfKeys) {
+        this.type = type;
+        this.numberOfKeys = numberOfKeys;
+    }
+
+    public String identity() {
+        return "I am the Keyboard class. I allow the user to input text and commands.\n";
+    }
+}
